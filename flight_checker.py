9ea50c9dbb5ee7dafb41d61c34040e7015e64f8a
Batirro <batirro34@protonmail.com>
2025-07-10 22:33:24 +0200

    Added email_sender.py to handle notification sending

--- flight_checker.py
+++ flight_checker.py
@@ -5,12 +5,13 @@ from dotenv import load_dotenv
 import os
 # Ładowanie zmienych z .env
 load_dotenv()
+
 rapid_api_key = os.getenv("RAPIDAPI_API")
 
 
 url = "https://google-flights4.p.rapidapi.com/date-grid/for-roundtrip"
 
-target_departure = "02.08.2025"  #input("Podaj datę wylotu (DD.MM.YYYY): ")
+target_departure = input("Podaj datę wylotu (DD.MM.YYYY): ")
 FormatedDate = datetime.strptime(target_departure, "%d.%m.%Y")
 date_obj = FormatedDate.strftime("%Y-%m-%d")
 FormatedArrivalDate = datetime.strptime(date_obj, "%Y-%m-%d") + timedelta(days=1)
