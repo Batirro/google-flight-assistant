e134d29b9433b725a81ad1a9db53068fb8ba3d1a
Batirro <batirro34@protonmail.com>
2025-08-01 20:32:57 +0200

    Changed API and rewrited api_connector to work with SerpAPI

--- app.py
+++ app.py
@@ -5,16 +5,19 @@ from logic.data_grabber import DataGrabber
 
 
 if __name__ == "__main__":
+    DataGrabber = DataGrabber()
+    FlightChecker = FlightChecker()
     month = input("Podaj miesiąc wylotu: ")
-    year = input("Podaj rok wylotu:")
-    target_departure, arrivalDate = DataParser.formatowanie_daty(month, year)
-    response = DataGrabber.api_connector(target_departure, arrivalDate)
+    year = input("Podaj rok wylotu: ")
+    TripLenght = int(input("Podaj długość wycieczki: "))
+    target_departure, return_date = DataParser.formatowanie_daty(month, year, TripLenght)
+    response = DataGrabber.api_connector(target_departure, return_date)
     DataGrabber.pobierz_dane(response)
     if FlightChecker.sprawdzanie_lotow(target_departure):
         subject = f'Znaleziono loty na {month} {year}'
         plainText = 'Znaleziono loty'
         print(f'Znaleziono loty na {month} {year}')
-        EmailSender.send_email(subject, plainText)
+        # EmailSender.send_email(subject, plainText)
     else:
         print(f'Nie ma jeszcze lotów dostępnych na {month} {year}')
 
--- logic/date_parser.py
+++ logic/date_parser.py
@@ -3,21 +3,6 @@ from datetime import datetime, timedelta
 class DataParser:
     @staticmethod
     def generuj_date_z_miesiaca(nazwa_miesiaca, rok):
-        """
-        Generates a date string in 'YYYY-MM-DD' format for the first day of a given month and year.
-        Args:
-            nazwa_miesiaca (str): Name of the month in Polish (e.g., 'Styczeń', 'Luty', etc.)
-            rok (int): Year as a number (e.g., 2023)
-        Returns:
-            str: Date string in 'YYYY-MM-DD' format representing the first day of the specified month and year
-        Raises:
-            ValueError: If the provided month name is not recognized
-        Examples:
-            >>> generuj_date_z_miesiaca('Styczeń', 2023)
-            '2023-01-01'
-            >>> generuj_date_z_miesiaca('Grudzień', 2024)
-            '2024-12-01'
-        """
         miesiace = {
             'Styczeń': 1, 'Luty': 2, 'Marzec': 3, 'Kwiecień': 4,
             'Maj': 5, 'Czerwiec': 6, 'Lipiec': 7, 'Sierpień': 8,
@@ -31,28 +16,13 @@ class DataParser:
             raise ValueError(f"Nieznana nazwa miesiąca: {nazwa_miesiaca}")
 
     @staticmethod
-    def formatowanie_daty(month, year):
-        """
-        Formats and generates a pair of consecutive dates based on given month and year.
-        Args:
-            month (str): Month name in Polish (e.g., 'styczeń', 'luty', etc.)
-            year (int): Year as a four-digit number
-        Returns:
-            tuple: A pair of consecutive dates (target_departure, arrival_date) in 'YYYY-MM-DD' format
-                - target_departure (str): The departure date generated from the given month and year
-                - arrival_date (str): The date following the departure date
-        Raises:
-            ValueError: If the date generation fails for the given month and year
-        Example:
-            >>> formatowanie_daty('styczeń', 2024)
-            ('2024-01-01', '2024-01-02')
-        """
+    def formatowanie_daty(month, year, TripLenght):
         target_departure = DataParser.generuj_date_z_miesiaca(month, year)
         if target_departure is None:
             raise ValueError("Nie udało się wygenerować daty.")
         
-        arrival_date = (datetime.strptime(target_departure, "%Y-%m-%d") 
-                       + timedelta(days=1))
-        arrival_date = arrival_date.strftime("%Y-%m-%d")
+        returnDate = (datetime.strptime(target_departure, "%Y-%m-%d") 
+                       + timedelta(days=TripLenght))
+        returnDate = returnDate.strftime("%Y-%m-%d")
         
-        return target_departure, arrival_date
+        return target_departure, returnDate
